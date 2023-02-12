789190b0a1f0515adaa90c1bb84df79a2923074d
Karlie Griffin <karliegriffin@Karlies-Mini.lan>
2023-02-12 11:22:46 -0600

    Adding home page view.

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello my name is Karlie!')
+    res.render('Home')
 })
 app.get('*', (req,res)=> {
     res.status(404) .send('<h1>404 This Page Cannot Be Found :( </h1>')
