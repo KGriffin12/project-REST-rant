aa43178f8ac3881517527ecf72c4d24bfc5d9c74
Karlie Griffin <karliegriffin@Karlies-Mini.lan>
2023-02-14 16:58:06 -0600

    Adding places view with mock data.

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,22 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req,res) => {
-    res.send('GET /places')
-})
+router.get("/", (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'https://images.travelandleisureasia.com/wp-content/uploads/sites/2/2022/05/25114414/Untitled-design-2022-05-25T114359.104.jpg?tr=w-250,h-250'
+    }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'https://patch.com/img/cdn20/users/1043655/20180509/045844/styles/raw/public/processed_images/coffee_cat_9-1525888754-5567.jpg?width=250'
+      }]
+      
 
-module.exports = router
+  res.render("places/index", { places });
+});
+
+module.exports = router;
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,14 +1,27 @@
-const React = require('react')
-const Def = require('./default')
+const React = require("react");
+const Def = require("../default");
 
-function home () {
+function index(data) {
+  let placesFormatted = data.places.map((place) => {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
-    )
-  }
-  
-module.exports = home
+      <div className="col-sm-6">
+        <h2>{place.name}</h2>
+        <p className="text-center">{place.cuisines}</p>
+        <img src={place.pic} alt={place.name} />
+        <p className="text-center">
+          Located in {place.city}, {place.state}
+        </p>
+      </div>
+    );
+  });
+  return (
+    <Def>
+      <main>
+        <h1>Places to Rant About</h1>
+        <div className="row">{placesFormatted}</div>
+      </main>
+    </Def>
+  );
+}
+
+module.exports = index;
