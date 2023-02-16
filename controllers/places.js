6903104d620628b9ae11f8fd72ddd9aa2b6202bd
Karlie Griffin <karliegriffin@Karlies-Mini.lan>
2023-02-16 17:44:14 -0600

    Added the edit form.

--- controllers/places.js
+++ controllers/places.js
@@ -41,6 +41,20 @@ router.get('/:id', (req, res) => {
   }
 });
 
+//edit
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
 
 //delete
 router.delete('/:id', (req, res) => {
