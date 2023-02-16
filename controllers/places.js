00a11f4d1e18c339b2acc8212948c632c48a042a
Karlie Griffin <karliegriffin@Karlies-Mini.lan>
2023-02-16 13:21:12 -0600

    New places can be added.

--- controllers/places.js
+++ controllers/places.js
@@ -1,41 +1,28 @@
 const router = require("express").Router();
-const places = require('../models/places.js')
+const places = require('../models/places.js');
 
-router.get("/", (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'https://images.travelandleisureasia.com/wp-content/uploads/sites/2/2022/05/25114414/Untitled-design-2022-05-25T114359.104.jpg?tr=w-800,h-550'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'https://patch.com/img/cdn20/users/1043655/20180509/045844/styles/raw/public/processed_images/coffee_cat_9-1525888754-5567.jpg?width=800'
-      }]
-      
-      router.get('/new', (req, res) => {
-        res.render('places/new');
-    });
+router.get('/', (req, res) => {
+    res.render("places/index", { places });
+  });
 
       router.post('/', (req, res) => {
+        console.log(req.body)
         if (!req.body.pic) {
           // Default image if one is not provided
-          req.body.pic = 'http://placekitten.com/400/400'
+          req.body.pic = "public/images/food-skillet.jpg";
         }
         if (!req.body.city) {
-          req.body.city = 'Anytown'
+          req.body.city = 'Anytown';
         }
         if (!req.body.state) {
-          req.body.state = 'USA'
+          req.body.state = 'USA';
         }
-        places.push(req.body)
-        res.redirect('/places')
-      })
+        places.push(req.body);
+        res.redirect('/places');
+      });
       
-  res.render("places/index", { places });
-});
 
+router.get('/new', (req, res) => {
+    res.render('places/new');
+});
 module.exports = router;
--- models/places.js
+++ models/places.js
@@ -3,11 +3,13 @@ module.exports = [{
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai-ml-tables.jpg'
-}, {
+    pic: '/images/coding-cafe.jpg'
+}, 
+{
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
     cuisines: 'Coffee, Bakery',
     pic: '/images/coffee-cat.jpg'
-}]
+},
+];
