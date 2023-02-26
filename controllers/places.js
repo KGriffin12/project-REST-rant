603089aff4a9737e338b44a73e75f99d81cc41b3
Karlie Griffin <karliegriffin@Karlies-Mini.lan>
2023-02-25 19:37:22 -0600

    added year founded.

--- controllers/places.js
+++ controllers/places.js
@@ -1,104 +1,65 @@
 const router = require("express").Router();
-const places = require('../models/places.js');
+const db = require("../models");
 
-//create
-      router.post('/', (req, res) => {
-        console.log(req.body)
-        if (!req.body.pic) {
-          // Default image if one is not provided
-          req.body.pic = "public/images/food-skillet.jpg";
-        }
-        if (!req.body.city) {
-          req.body.city = 'Anytown';
-        }
-        if (!req.body.state) {
-          req.body.state = 'USA';
-        }
-        places.push(req.body);
-        res.redirect('/places');
-      });
-//
-router.get('/', (req, res) => {
-  res.render("places/index", { places });
+// index
+router.get("/", (req, res) => {
+  db.Place.find()
+    .then((places) => {
+      res.render("places/index", { places });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render('error404');
+    });
 });
 
-//new
-router.get('/new', (req, res) => {
-    res.render('places/new');
+// create
+router.post("/", (req, res) => {
+  db.Place.create(req.body)
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
 
-//show
-router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
+//  new
+router.get("/new", (req, res) => {
+  res.render("places/new");
 });
 
-//edit
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id] })
-  }
+//  show
+router.get("/:id", (req, res) => {
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      res.render("places/show", { place });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
 
-// PUT ROUTE
-router.put('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-      // Dig into req.body and make sure data is valid
-      if (!req.body.pic) {
-          // Default image if one is not provided
-          req.body.pic = 'http://placekitten.com/400/400'
-      }
-      if (!req.body.city) {
-          req.body.city = 'Anytown'
-      }
-      if (!req.body.state) {
-          req.body.state = 'USA'
-      }
+// delete route
+router.delete("/:id", (req, res) => {
+  db.Place.deleteOne({ id: req.params.id })
+    .then(() => res.redirect("/places"))
+    .catch((err) => {
+      console.log(err);
+      res.status(404).render("error404");
+    });
+});
 
-      // Save the new data into places[id]
-      places[id] = req.body
-      res.redirect(`/places/${id}`)
-  }
-})
+//  put
+router.put("/:id", (req, res) => {
+  res.send("PUT /places/:id stub");
+});
 
-
-
-
-//delete
-router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.send('/places');
-  }
+// edit
+router.get("/:id/edit", (req, res) => {
+  res.send("GET edit form stub");
 });
 
 module.exports = router;
